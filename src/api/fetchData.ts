07ddb62b547752ae3da7b8ad5c36300c83dda4ab
SergeyFF3 <specialforce64044@gmail.com>
2025-03-04 15:40:05 +0300

    added types, request for get matches

---
+++ src/api/fetchData.ts
@@ -0,0 +1,25 @@
+import { MatchType } from "../types/types";
+
+const baseUrl = "https://app.ftoyd.com/fronttemp-service";
+
+export const getMatches = async () => {
+  try {
+    const response = await fetch(`${baseUrl}/fronttemp`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      console.log("Ошибка загрузки матчей", response.statusText);
+      return null;
+    }
+
+    const result = await response.json();
+    return result.data.matches as MatchType[];
+  } catch {
+    console.log("Ошибка: не удалось загрузить информацию");
+    return null;
+  }
+};
---
+++ src/types/types.ts
@@ -0,0 +1,22 @@
+export type PlayerType = {
+  kills: number;
+  username: string;
+};
+
+export type TeamType = {
+  name: string;
+  place: number;
+  points: number;
+  total_kills: number;
+  players: PlayerType[];
+};
+
+export type MatchType = {
+  awayScore: number;
+  homeScore: number;
+  status: string;
+  time: string;
+  title: string;
+  awayTeam: TeamType;
+  homeTeam: TeamType;
+};
